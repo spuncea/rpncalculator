090a069a98296df7826a4357dfab19dba2ae3000
Florin Puncea <f.puncea@mailxpertise.com>
2020-08-02 18:12:49 +0300

    [code] added required validation methods and constants

--- src/Utils/Regexp.php
+++ src/Utils/Regexp.php
@@ -14,4 +14,20 @@ final class Regexp
     {
         // singleton 
     }
+
+    /**
+     * Regular expression to validate a numeric value
+     */
+    const NUMERIC = '/^(\d)*$/';
+
+    /**
+     * Regular expression to validate a fractional value
+     */
+    const FRACTIONAL_NUMBER = '/^(?!-0(\.0+)?$)-?(0|[1-9]\d*)(\.\d+)?$/';
+
+    /**
+     * Regular expression to validate a numeric strict negative value
+     */
+    const NUMERIC_STRICT_NEGATIVE = '/^-(\d)*$/';
+
 }
--- src/Utils/Validate.php
+++ src/Utils/Validate.php
@@ -11,4 +11,22 @@ final class Validate
         // nothing to do
     }
 
+    /**
+     * Check if subject matches pattern
+     *
+     * @param string $subject String to search the pattern in
+     * @param string $pattern The string pattern to be searched in $subject
+     * @return boolean
+     */
+    public static function regexp($subject, $pattern)
+    {
+        $pattern = '/' . trim($pattern, '/') . '/';
+
+        if (0 === preg_match($pattern, (string) $subject)) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
