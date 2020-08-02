837144d17bf3d53f43056df96f4da9bf59cfc4d5
Florin Puncea <f.puncea@mailxpertise.com>
2020-08-02 18:14:25 +0300

    [code] added basic operators

--- src/AbstractCalculator.php
+++ src/AbstractCalculator.php
@@ -3,5 +3,25 @@
 namespace RPN; 
 
 abstract class AbstractCalculator {
+
+    /**
+     * Addition operator
+     */
+    const OPERATOR_ADDITION = '+';
     
+    /**
+     * Substraction operator
+     */
+    const OPERATOR_SUBSTRACTION = '-';
+
+    /**
+     * Division operator
+     */
+    const OPERATOR_DIVISON = '/';
+    
+    /**
+     * Multiplication operator
+     */
+    const OPERATOR_MULTIPLICATION = '*';
+
 }
